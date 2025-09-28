ed71ddf4f225e4a268190148e8f04ce5dd58a9c2
Abhishek1017-sh <as20041009@gmail.com>
2025-09-28 14:06:10 +0530

    Time: 314 ms (5.8%), Space: 45.2 MB (40%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.java
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.java
@@ -1,17 +1,28 @@
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
-        int totalDistinct = (int) Arrays.stream(nums).distinct().count();
-        int res = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            Set<Integer> set = new HashSet<>();
-            for (int j = i; j < n; j++) {
-                set.add(nums[j]);
-                if (set.size() == totalDistinct) {
-                    res++;
+        HashSet<Integer> map=new HashSet<>();
+        for(int e:nums){
+            map.add(e);
+        }
+        int dis=map.size();
+        int count=0;
+        for(int k=dis;k<=nums.length;k++){
+            int i=0;
+            int j=0;
+            HashMap<Integer,Integer> check=new HashMap<>();
+            while(j<nums.length){
+                check.put(nums[j],check.getOrDefault(nums[j],0)+1);
+                if(j-i+1==k){
+                    if(check.size()==dis) count++;
+                    check.put(nums[i],check.get(nums[i])-1);
+                    if(check.get(nums[i])==0){
+                        check.remove(nums[i]);
+                    }
+                    i++;
                 }
+                j++;
             }
         }
-        return res;
+        return count;
     }
 }
