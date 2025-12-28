20a846a53d7a226aa3b57d60056ac9ae89763c11
Abhishek1017-sh <as20041009@gmail.com>
2025-12-28 23:41:34 +0530

    Time: 40 ms (27.19%), Space: 66.1 MB (94.93%) - LeetHub

---
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
+        int ans = 0, j = -1;
+        long s = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(b[0], a[0]));
+        for (int i = 0; i < runningCosts.length; i++) {
+            s += runningCosts[i];
+            pq.offer(new int[] {chargeTimes[i], i});
+            while (s * (i - j) + removeStale(pq, j) > budget){
+                s -= runningCosts[++j];
+            }
+            ans = Math.max(ans, i - j);
+        }
+        return ans;
+    }
+    static int removeStale(PriorityQueue<int[]> pq, int j) {
+        while (!pq.isEmpty() && pq.peek()[1] <= j){
+            pq.poll();
+        }
+        if (pq.isEmpty()){
+            return 0;
+        }
+        return pq.peek()[0];
+    }
+}
