55991d9fdcc3b400e2bb12998215104ebf1e5cb8
Abhishek1017-sh <as20041009@gmail.com>
2025-12-30 16:12:16 +0530

    Time: 108 ms (5.64%), Space: 189.1 MB (5.13%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -1,43 +1,49 @@
 class Solution {
-    public void bfs(int startNode, int[] edges, int[] dist) {
-        int n = edges.length;
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(startNode);
-
-        Boolean[] visit = new Boolean[n];
-        Arrays.fill(visit, Boolean.FALSE);
-        dist[startNode] = 0;
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-
-            if (visit[node]) {
-                continue;
+    public static void dfs(int node,ArrayList<ArrayList<Integer>> adj,int dis[],int vis[],int d){
+        vis[node]=1;
+        dis[node] = Math.min(d,dis[node]);
+        for(int no:adj.get(node)){
+            if(vis[no]!=1){
+                dfs(no,adj,dis,vis,dis[node]+1);
             }
-
-            visit[node] = true;
-            int neighbor = edges[node];
-            if (neighbor != -1 && !visit[neighbor]) {
-                dist[neighbor] = 1 + dist[node];
-                q.offer(neighbor);
-            }
-
         }
     }
     public int closestMeetingNode(int[] edges, int node1, int node2) {
         int n = edges.length;
-        int[] dist1 = new int[n], dist2 = new int[n];
-        Arrays.fill(dist1, Integer.MAX_VALUE);
-        Arrays.fill(dist2, Integer.MAX_VALUE);
-        bfs(node1, edges, dist1);
-        bfs(node2, edges, dist2);
-        int minDistNode = -1, minDistTillNow = Integer.MAX_VALUE;
-        for (int currNode = 0; currNode < n; currNode++) {
-            if (minDistTillNow > Math.max(dist1[currNode], dist2[currNode])) {
-                minDistNode = currNode;
-                minDistTillNow = Math.max(dist1[currNode], dist2[currNode]);
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++){
+            int v = edges[i];
+            if(v!=-1){
+                adj.get(i).add(v);
+            }            
+        }
+        int vis[] = new int[n];
+        int dist1[] = new int[n];
+        int dist2[]  = new int[n];
+        Arrays.fill(dist1,Integer.MAX_VALUE);
+        dfs(node1,adj,dist1,vis,0);
+
+        Arrays.fill(vis,0);
+        Arrays.fill(dist2,Integer.MAX_VALUE);
+        dfs(node2,adj,dist2,vis,0);
+        int dis[] = new int[n];
+        for(int i=0;i<n;i++){
+            dis[i] = Math.max(dist1[i],dist2[i]);
+        }
+        int ans = -1;
+        int best = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(dist1[i] != Integer.MAX_VALUE && dist2[i] != Integer.MAX_VALUE){
+                int max = Math.max(dist1[i], dist2[i]);
+                if(max < best){
+                    best = max;
+                    ans = i;
+                }
             }
         }
-        return minDistNode; 
+        return ans;
     }
 }
