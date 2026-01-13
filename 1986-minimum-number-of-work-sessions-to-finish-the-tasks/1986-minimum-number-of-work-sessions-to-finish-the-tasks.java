fe97c6b0fcb4c1225ad3de3e631e9e9cda3c55aa
Abhishek1017-sh <as20041009@gmail.com>
2026-01-13 22:24:17 +0530

    Time: 94 ms (41.22%), Space: 44.4 MB (40.46%) - LeetHub

--- 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.java
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.java
@@ -1,18 +1,27 @@
 class Solution {
     public int minSessions(int[] tasks, int sessionTime) {
-        Arrays.sort(tasks);
-        int cnt = 1;
-        int time = sessionTime;
-        for (int i = tasks.length - 1; i >= 0; i--) {
-            if (sessionTime - tasks[i] >= 0) {
-                sessionTime -= tasks[i];
-            } 
-            else {
-                cnt++;
-                sessionTime = time;
-                sessionTime -= tasks[i];
+        int n = tasks.length;
+        int size = 1 << n;
+        boolean[] valid = new boolean[size];
+        for (int mask = 0; mask < size; mask++) {
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    sum += tasks[i];
+                }
+            }
+            valid[mask] = sum <= sessionTime;
+        }
+        int[] dp = new int[size];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int mask = 1; mask < size; mask++) {
+            for (int sub = mask; sub > 0; sub = (sub - 1) & mask) {
+                if (valid[sub]) {
+                    dp[mask] = Math.min(dp[mask], dp[mask ^ sub] + 1);
+                }
             }
         }
-        return cnt;
+        return dp[size - 1];
     }
 }
