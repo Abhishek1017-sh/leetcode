4d26b8e8f8237282ada0ba40af9975470b45eecc
Abhishek1017-sh <as20041009@gmail.com>
2026-02-18 13:11:11 +0530

    Time: 28 ms (22.32%), Space: 54.3 MB (72.21%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,46 +1,38 @@
 class Solution {
-    static boolean bfs(int node,ArrayList<ArrayList<Integer>> adj,Set<Integer> set1,Set<Integer> set2,int[] visited){
-        visited[node]=1;
+    static boolean possible(Map<Integer,List<Integer>> adj,int node,int[] color){
         Queue<Integer> que=new LinkedList<>();
         que.add(node);
-        set1.add(node);
+        color[node]=1;
         while(!que.isEmpty()){
-            int pol=que.poll();
-            for(int nei:adj.get(pol)){
-                if(visited[nei]!=1 && set1.contains(pol)){
-                    set2.add(nei);
-                    que.add(nei);
-                    visited[nei]=1;
-                }
-                else if(visited[nei]!=1 && set2.contains(pol)){
-                    set1.add(nei);
-                    que.add(nei);
-                    visited[nei]=1;
-                }
-                else if((set1.contains(pol) && set1.contains(nei)) || (set2.contains(pol) && set2.contains(nei))){
+            int po=que.poll();
+            for(int i:adj.get(po)){
+                if(color[i]==color[po]){
                     return false;
+                }
+                else if(color[i]==-1){
+                    que.add(i);
+                    color[i]=1-color[po];
                 }
             }
         }
         return true;
     }
     public boolean possibleBipartition(int n, int[][] dislikes) {
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        Set<Integer> set1=new HashSet<>();
-        Set<Integer> set2=new HashSet<>();
-        int[] visited=new int[n+1];
-        for(int i=0;i<=n;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int[] it:dislikes){
-            int u=it[0];
-            int v=it[1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-        }
+        Map<Integer,List<Integer>> adj=new HashMap<>();
         for(int i=1;i<=n;i++){
-            if(visited[i]!=1){
-                if(!bfs(i,adj,set1,set2,visited)){
+            adj.put(i,new ArrayList<>());
+        }
+        for(int[] i:dislikes){
+            int a=i[0];
+            int b=i[1];
+            adj.get(a).add(b);
+            adj.get(b).add(a);
+        }
+        int[] color=new int[n+1];
+        Arrays.fill(color,-1);
+        for(int i=1;i<=n;i++){
+            if(color[i]==-1){
+                if(!possible(adj,i,color)){
                     return false;
                 }
             }
