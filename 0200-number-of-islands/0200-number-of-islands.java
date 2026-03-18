50123c767c7376b46fd5d245491b54137b4d19fa
Abhishek1017-sh <as20041009@gmail.com>
2026-03-18 11:36:06 +0530

    Time: 7 ms (14.32%), Space: 55 MB (5.29%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,27 +1,48 @@
+class Pair{
+    int first;
+    int second;
+    public Pair(int first,int second){
+        this.first=first;
+        this.second=second;
+    }
+}
 class Solution {
     public int numIslands(char[][] grid) {
-        if(grid==null || grid.length==0) return 0;
-        int m=grid.length;
-        int n=grid[0].length;
+        int rows=grid.length;
+        int cols=grid[0].length;
+        int[][] vis=new int[rows][cols];
         int cnt=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='1'){
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(vis[i][j]==0 && grid[i][j]=='1'){
                     cnt++;
-                    dfs(grid,i,j);
+                    bfs(vis,grid,i,j);
                 }
             }
         }
         return cnt;
     }
-    static void dfs(char[][] grid,int i,int j){
+    static void bfs(int[][] vis,char[][] grid,int i,int j){
+        vis[i][j]=1;
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(i,j));
         int rows=grid.length;
         int cols=grid[0].length;
-        if(i<0 || i>=rows || j<0 || j>=cols || grid[i][j]=='0') return;
-        grid[i][j]='0';
-        dfs(grid,i+1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j-1);
+        while(!q.isEmpty()){
+            int row=q.peek().first;
+            int col=q.peek().second;
+            q.remove();
+            int[] dr={-1,0,1,0};
+            int[] dc={0,-1,0,1};
+            for(int b=0;b<4;b++){
+                int nrow=row+dr[b];
+                int ncol=col+dc[b];
+                if(nrow>=0 && nrow<rows && ncol>=0 && ncol<cols &&
+                vis[nrow][ncol]==0 && grid[nrow][ncol]=='1'){
+                    vis[nrow][ncol]=1;
+                    q.add(new Pair(nrow,ncol));
+                }
+            }
+        }
     }
 }
