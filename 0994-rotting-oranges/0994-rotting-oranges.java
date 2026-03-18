f809f43b504c75f09dcfdb1f0235a5cd3f86ceef
Abhishek1017-sh <as20041009@gmail.com>
2026-03-18 14:47:04 +0530

    Time: 2 ms (90.45%), Space: 44.3 MB (35.75%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,40 +1,56 @@
+class Pair{
+    int first;
+    int second;
+    int tm;
+    Pair(int first,int second,int tm){
+        this.first=first;
+        this.second=second;
+        this.tm=tm;
+    }
+}
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        int freshOranges = 0;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == 2) {
-                    q.offer(new int[]{i, j});
-                } else if (grid[i][j] == 1) {
-                    freshOranges++;
+        int m=grid.length;
+        int n=grid[0].length;
+        Queue<Pair> q=new LinkedList<>();
+        int[][] vis=new int[m][n];
+        int cntfresh=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.add(new Pair(i,j,0));
+                    vis[i][j]=2;
+                }
+                else{
+                    vis[i][j]=0;
+                }
+                if(grid[i][j]==1) cntfresh++;
+            }
+        }
+        int tm=0;
+        int[] dr={-1,0,1,0};
+        int[] dc={0,-1,0,1};
+        int cnt=0;
+        while(!q.isEmpty()){
+            int r=q.peek().first;
+            int c=q.peek().second;
+            int time=q.peek().tm;
+            tm=Math.max(time,tm);
+            q.remove();
+            for(int i=0;i<4;i++){
+                int nrow=r+dr[i];
+                int ncol=c+dc[i];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 
+                && grid[nrow][ncol]==1){
+                    q.add(new Pair(nrow,ncol,tm+1));
+                    vis[nrow][ncol]=2;
+                    cnt++;
                 }
             }
         }
-
-        int minutes = 0;
-        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-        while (!q.isEmpty() && freshOranges > 0) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int[] curr = q.poll();
-                for (int[] dir : dirs) {
-                    int nextRow = curr[0] + dir[0];
-                    int nextCol = curr[1] + dir[1];
-                    if (nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols && grid[nextRow][nextCol] == 1) {
-                        grid[nextRow][nextCol] = 2;
-                        q.offer(new int[]{nextRow, nextCol});
-                        freshOranges--;
-                    }
-                }
-            }
-            minutes++;
+        if(cnt!=cntfresh){
+            return -1;
         }
-
-        return freshOranges == 0 ? minutes : -1;
+        return tm;
     }
 }
