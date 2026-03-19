215a9964e24d05bf4418fe5b47948b355012ba21
Abhishek1017-sh <as20041009@gmail.com>
2026-03-19 12:06:59 +0530

    Time: 15 ms (45.11%), Space: 50 MB (7.44%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,36 +1,49 @@
+class Pair{
+    int first;
+    int second;
+    int dist;
+    Pair(int first,int second,int dist){
+        this.first=first;
+        this.second=second;
+        this.dist=dist;
+    }
+}
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int n=mat.length;
-        int m=mat[0].length;
-        Queue<int[]> q=new LinkedList<>();
-        int[][] dist=new int[n][m];
-        for(int[] d:dist){
-            Arrays.fill(d,Integer.MAX_VALUE);
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int m=mat.length;
+        int n=mat[0].length;
+        int[][] vis=new int[m][n];
+        int[][] dist=new int[m][n];
+        Queue<Pair> q=new LinkedList<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
-                    dist[i][j]=0;
-                    q.add(new int[]{i,j,0});
+                    q.add(new Pair(i,j,0));
+                    vis[i][j]=1;
+                }
+                else{
+                    vis[i][j]=0;
                 }
             }
         }
-        int[] dr={-1,0,0,1};
-        int[] dc={0,-1,1,0};
+        int[] dr={-1,0,1,0};
+        int[] dc={0,-1,0,1};
         while(!q.isEmpty()){
-            int[] poll=q.poll();
-            int x=poll[0], y=poll[1], di=poll[2];
+            int row=q.peek().first;
+            int col=q.peek().second;
+            int d=q.peek().dist;
+            q.remove();
+            dist[row][col]=d;
             for(int i=0;i<4;i++){
-                int nr=x+dr[i];
-                int nc=y+dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && mat[nr][nc]==1){
-                    if(dist[nr][nc]>di+1){
-                        dist[nr][nc]=di+1;
-                        q.add(new int[]{nr,nc,di+1});
-                    }
+                int nrow=row+dr[i];
+                int ncol=col+dc[i];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 
+                && mat[nrow][ncol]==1){
+                    vis[nrow][ncol]=1;
+                    q.add(new Pair(nrow,ncol,d+1));
                 }
             }
         }
         return dist;
-    }
+   }
 }
