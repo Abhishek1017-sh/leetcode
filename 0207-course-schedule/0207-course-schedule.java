cf62fefd427a48c3eaddd5402e5473813ffb28ca
Abhishek1017-sh <as20041009@gmail.com>
2026-03-24 12:21:53 +0530

    Time: 7 ms (60.42%), Space: 47.4 MB (17.44%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,36 @@
 class Solution {
     public boolean canFinish(int V,int[][] edges) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
-        int indegree[] = new int[V];
-        for(int edge[] :edges){
-            int u = edge[0];
-            int v = edge[1];
-            adj.get(v).add(u);
-            indegree[u]++;
+        int m=edges.length;
+        for(int i=0;i<m;i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
         }
-        Queue<Integer> queue = new LinkedList<>();
-        int visited[] = new int[V];
+        int[] indeg=new int[V];
         for(int i=0;i<V;i++){
-            if(indegree[i] == 0){
-                queue.offer(i);
-                visited[i] = 1;
+            for(int it:adj.get(i)){
+                indeg[it]++;
             }
         }
-        while(!queue.isEmpty()){
-            int node = queue.poll();
-            visited[node] =1;
-            for(int ele:adj.get(node)){
-                indegree[ele]--;
-                if(indegree[ele] == 0){
-                    queue.offer(ele);
-                }
-            }
-        }
+        Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<V;i++){
-            if(visited[i] == 0){
-                return false;
+            if(indeg[i]==0){
+                q.add(i);
             }
         }
-        return true;
+        List<Integer> list=new ArrayList<>();
+        while(!q.isEmpty()){
+            int node=q.peek();
+            q.remove();
+            list.add(node);
+            for(int it:adj.get(node)){
+                indeg[it]--;
+                if(indeg[it]==0) q.add(it);
+            }
+        }
+        if(list.size()==V) return true;
+        return false;
     }
 }
