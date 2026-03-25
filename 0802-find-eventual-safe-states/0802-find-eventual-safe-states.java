b894613aad649b8bb880ed79863cb53c420c7d0f
Abhishek1017-sh <as20041009@gmail.com>
2026-03-26 00:08:38 +0530

    Time: 27 ms (31.4%), Space: 60.6 MB (78.17%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,34 +1,34 @@
 class Solution {
-    private boolean dfs(int node,int[][] graph,int[] vis,int[] pathvis,int[] check){
-        vis[node]=1;
-        pathvis[node]=1;
-        check[node]=0;
-        for(int nei:graph[node]){
-            if(vis[nei]==0){
-                if(dfs(nei,graph,vis,pathvis,check)==true) return true; 
-            }
-            else if(pathvis[nei]==1){
-                return true;
-            }
-        }
-        check[node]=1;
-        pathvis[node]=0;
-        return false;
-    }
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n=graph.length;
-        int[] vis=new int[n];
-        int[] pathvis=new int[n];
-        int[] check=new int[n];
+        List<List<Integer>> adjrev=new ArrayList<>();
         for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                dfs(i,graph,vis,pathvis,check);
+            adjrev.add(new ArrayList<>());
+        }
+        int[] indeg=new int[n];
+        for(int i=0;i<n;i++){
+            for(int it:graph[i]){
+                adjrev.get(it).add(i);
+                indeg[i]++;
             }
         }
-        List<Integer> list=new ArrayList<>();
+        Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<n;i++){
-            if(check[i]==1) list.add(i);
+            if(indeg[i]==0){
+                q.add(i);
+            }
         }
-        return list;
+        List<Integer> ans=new ArrayList<>();
+        while(!q.isEmpty()){
+            int node=q.peek();
+            q.remove();
+            ans.add(node);
+            for(int it:adjrev.get(node)){
+                indeg[it]--;
+                if(indeg[it]==0) q.add(it);
+            }
+        }
+        Collections.sort(ans);
+        return ans;
     }
 }
