c8f718e736a4b656e2de6deeb79f33540e138324
Abhishek1017-sh <as20041009@gmail.com>
2026-03-27 23:14:40 +0530

    Time: 97 ms (30.87%), Space: 48.8 MB (35.55%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,26 +1,32 @@
+class Pair{
+    String first;
+    int second;
+    Pair(String first,int second){
+        this.first=first;
+        this.second=second;
+    }
+}
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> set=new HashSet<>();
-        Queue<Object[]> que=new LinkedList<>();
+        Queue<Pair> que=new LinkedList<>();
         for(String i:wordList){
             set.add(i);
         }
-        que.add(new Object[]{beginWord,1});
+        que.add(new Pair(beginWord,1));
         set.remove(beginWord);
         while(!que.isEmpty()){
-            Object[] pair=que.poll();
-            String word=(String)pair[0];
-            int cnt=(int)pair[1];
+            String word=que.peek().first;
+            int cnt=que.peek().second;
+            que.remove();
+            if(word.equals(endWord)==true) return cnt;
             for(int i=0;i<word.length();i++){
                 for(char j='a';j<='z';j++){
                     char[] split_word=word.toCharArray();
                     split_word[i]=j;
                     String reword=new String(split_word);
                     if(set.contains(reword)){
-                        if(reword.equals(endWord)){
-                            return cnt+1;
-                        }
-                        que.add(new Object[]{reword,cnt+1});
+                        que.add(new Pair(reword,cnt+1));
                         set.remove(reword);
                     }
                 }
